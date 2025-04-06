fe190a0d8b811bb7eaffbfb580410bca0ec06272
joaopaulopereirarezendesesi <joaopp.rezende@gmail.com>
2025-04-06 16:24:44 -0300

    update

--- backend/server/index.js
+++ backend/server/index.js
@@ -85,7 +85,7 @@ const routes = require("./routes");
 app.use("/api", routes);
 
 // Configurando o servidor para escutar em uma porta específica
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
