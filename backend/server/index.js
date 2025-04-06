54d98d716df9fac68aa6b98b05b5a1176e4f2ab9
joaopaulopereirarezendesesi <joaopp.rezende@gmail.com>
2025-04-06 16:24:08 -0300

    update

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,7 +1,21 @@
+// Carregando as dependências necessárias
+const cors = require("cors");
 const express = require("express");
+
+// Carregando as variáveis de ambiente do arquivo .env
+require("dotenv").config();
+
+// Criando uma instância do express
 const app = express();
 
+//  Habilitando interpretação de corpo de requisições como JSON.
+app.use(express.json());
+// Habilitando interpretação de corpo de requisições como URL-encoded.
+app.use(express.urlencoded({ extended: true }));
+
+// Configurando o CORS
 class CORS {
+  // Construtor da classe CORS
   constructor(
     allowedOrigins = ["http://localhost:5173"],
     allowedMethods = ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -12,18 +26,24 @@ class CORS {
     this.allowedHeaders = allowedHeaders;
   }
 
+  // Método para configurar o CORS
   configure() {
+    // Middleware para lidar com CORS
     return (req, res, next) => {
+      // Verifica se o cabeçalho Origin está presente na requisição
       const origin = req.get("Origin");
 
+      // Se o cabeçalho Origin estiver presente, verifica se ele está na lista de origens permitidas
       if (this.allowedOrigins.includes(origin)) {
         res.setHeader("Access-Control-Allow-Origin", origin);
         res.setHeader("Access-Control-Allow-Credentials", "true");
       } else {
+        // Se a origem não estiver na lista de permitidas, retorna um erro 403
         res.status(403).send("Origem não permitida.");
         return;
       }
 
+      // Configura os cabeçalhos de resposta para CORS
       res.setHeader(
         "Access-Control-Allow-Methods",
         this.allowedMethods.join(", ")
@@ -33,26 +53,38 @@ class CORS {
         this.allowedHeaders.join(", ")
       );
 
+      // Configurando preflight requests (Requisição feita antes da requisição principal)
       if (req.method === "OPTIONS") {
-        res.setHeader('Access-Control-Allow-Origin', req.get('Origin'));
-        res.setHeader('Access-Control-Allow-Methods', this.allowedMethods.join(', '));
-        res.setHeader('Access-Control-Allow-Headers', this.allowedHeaders.join(', '));
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader("Access-Control-Allow-Origin", req.get("Origin"));
+        res.setHeader(
+          "Access-Control-Allow-Methods",
+          this.allowedMethods.join(", ")
+        );
+        res.setHeader(
+          "Access-Control-Allow-Headers",
+          this.allowedHeaders.join(", ")
+        );
+        res.setHeader("Access-Control-Allow-Credentials", "true");
         res.status(204).send();
       } else {
+        // Se não for uma requisição preflight, continua com o processamento normal
         next();
       }
     };
   }
 }
 
+// Instanciando a classe CORS e aplicando o middleware ao aplicativo Express
 const cors = new CORS();
 app.use(cors.configure());
 
-app.get("/", (req, res) => {
-  res.send("opa! Tudo certo por aqui!");
-});
+// Importando as rotas
+const routes = require("./routes");
 
+// Passando o middleware de rotas para o aplicativo Express
+app.use("/api", routes);
+
+// Configurando o servidor para escutar em uma porta específica
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
